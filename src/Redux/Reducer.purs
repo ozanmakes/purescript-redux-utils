78359fd0b010228314269c234fa36d74bd973d49
Ozan Sener <ozan@ozansener.com>
2016-03-23 19:04:16 +0200

    Use newtype for ReduxReducer

--- src/Redux/Reducer.purs
+++ src/Redux/Reducer.purs
@@ -1,19 +1,22 @@
 module Redux.Reducer where
 
+import Prelude (($), (<<<))
+
 import Data.Function (Fn2, mkFn2)
 import Data.Nullable (Nullable, toMaybe)
 import Data.Maybe (Maybe(Just))
 
 import Redux.Action (Action(Action))
 
-type ReduxReducer action state = Fn2 (Nullable state) (Action action) state
 type Reducer action state = action -> state -> state
+newtype ReduxReducer action state = ReduxReducer (Fn2 (Nullable state) (Action action) state)
 
 foreign import applyReducer :: forall a s. Reducer a s -> a -> s -> s
 
 reducer :: forall a s. Reducer a s -> s -> ReduxReducer a s
 reducer f initialState =
-  mkFn2 \state (Action action) ->
+  ReduxReducer <<< mkFn2 $
+  \state (Action action) ->
     case (toMaybe state) of
-      (Just s)  -> applyReducer f action.type s
+      (Just s) -> applyReducer f action.type s
       otherwise -> initialState
